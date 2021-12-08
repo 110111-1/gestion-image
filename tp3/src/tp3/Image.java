b7b92cb1839f3e3e725ea4306694f6a64ba1ed83
110111-1 <53265033+110111-1@users.noreply.github.com>
2021-12-08 17:05:37 +0100

    Add files via upload

---
+++ tp3/src/tp3/Image.java
@@ -0,0 +1,266 @@
+package tp3;
+
+
+public class Image {
+ //TP1
+ private Pixel[][] tabPixel;
+ private int hauteur;
+ private int largeur;
+
+ public Image(String path){ 
+  this.largeur=ImageUtil.readImage(path).length; 
+  this.hauteur=ImageUtil.readImage(path)[0].length;
+  this.tabPixel = new Pixel[largeur][hauteur];
+  int [][][] tab = ImageUtil.readImage(path);
+  for(int col=0;col<=largeur-1;col++){
+   for (int ligne=0;ligne<=hauteur-1;ligne++){
+    tabPixel[col][ligne] = new Pixel(tab[col][ligne][0],tab[col][ligne][1],tab[col][ligne][2],tab[col][ligne][3]);
+   }
+  }
+ }
+ public Image(){
+
+ }
+
+ public int getHauteur() {
+  return hauteur;
+ }
+
+ public int getLargeur() {
+  return largeur;
+ }
+
+ public Pixel[][] getTabPixel() {
+  return tabPixel;
+ }
+
+ private int[][][] export() {
+  int[][][] tab = new int[tabPixel.length][tabPixel[0].length][4];
+  for(int i=0;i<=tabPixel.length-1;i++) {
+   for(int j=0;j<=tabPixel[0].length-1;j++) {
+    tab[i][j][0]=tabPixel[i][j].getRouge();
+    tab[i][j][1]=tabPixel[i][j].getVert();
+    tab[i][j][2]=tabPixel[i][j].getBleu();
+    tab[i][j][3]=tabPixel[i][j].getAlpha();
+   }
+  }
+  return tab;
+ }
+
+ public void sauvegarde(String nomNouveauFichier, String type){
+  ImageUtil.writeImage(export(),"png","images/"+nomNouveauFichier+"."+type);
+ }
+
+ public void afficheImage(){
+  ImageUtil.afficheImage((export()));
+ }
+
+ public String toString() {
+  return "Nombre de pixel de l'image : "+(getHauteur()*getLargeur())+"\nDimensions : hauteur "+getHauteur()+" pixels, Largeur : "+getLargeur()+" pixels" ;
+ }
+ //TP2
+
+ public Image imageNB(){// TP 2 Question 1
+  Image imageNB = new Image();
+  imageNB.hauteur = getHauteur();
+  imageNB.largeur = getLargeur();
+  imageNB.tabPixel = new Pixel[largeur][hauteur];
+  for(int col=0;col<=largeur-1;col++){
+   for (int ligne=0;ligne<=hauteur-1;ligne++){
+    int valNB=(tabPixel[col][ligne].getRouge()+tabPixel[col][ligne].getVert()+tabPixel[col][ligne].getBleu())/3;
+    imageNB.tabPixel[col][ligne]= new Pixel(valNB, valNB, valNB, tabPixel[col][ligne].getAlpha());
+   }
+  }
+  return imageNB;
+ }
+
+ public void toNB(){//TP2 Question 2
+  for (int col = 0; col <= largeur-1; col++) {
+   for (int ligne = 0; ligne <= hauteur-1; ligne++) {
+    int valNB=(tabPixel[col][ligne].getRouge()+tabPixel[col][ligne].getVert()+tabPixel[col][ligne].getBleu())/3;
+    tabPixel[col][ligne].setPixel(valNB, valNB, valNB, tabPixel[col][ligne].getAlpha());
+   }
+  }
+ }
+
+ public void modifierComposante(char composante,int variation){ //TP2 Question 3
+  switch (composante){
+  case 'R' :
+   for (int col = 0; col <= largeur-1; col++) {
+    for (int ligne = 0; ligne <= hauteur-1; ligne++) {
+     int var = tabPixel[col][ligne].getRouge()+((tabPixel[col][ligne].getRouge()*variation)/100);
+     if(var<0){
+      tabPixel[col][ligne].setRouge(0); 
+     }
+     else if(var>255){
+      tabPixel[col][ligne].setRouge(255);
+     }
+     else{
+      tabPixel[col][ligne].setRouge(var);
+     }
+    }
+   }
+   break;
+  case 'V' :
+   for (int col = 0; col <= largeur-1; col++) {
+    for (int ligne = 0; ligne <= hauteur-1; ligne++) {
+     int var = tabPixel[col][ligne].getVert()+((tabPixel[col][ligne].getVert()*variation)/100);
+     if(var<0){
+      tabPixel[col][ligne].setVert(0); 
+     }
+     else if(var>255){
+      tabPixel[col][ligne].setVert(255);
+     }
+     else{
+      tabPixel[col][ligne].setVert(var);
+     }
+    }
+   }
+   break;
+  case 'B':
+   for (int col = 0; col <= largeur-1; col++) {
+    for (int ligne = 0; ligne <= hauteur-1; ligne++) {
+     int var = tabPixel[col][ligne].getBleu()+((tabPixel[col][ligne].getBleu()*variation)/100);
+     if(var<0){
+      tabPixel[col][ligne].setBleu(0); 
+     }
+     else if(var>255){
+      tabPixel[col][ligne].setBleu(255);
+     }
+     else{
+      tabPixel[col][ligne].setBleu(var);
+     }
+    }
+   }
+   break;
+  default : 
+   System.out.println("Erreur dans le choix de composante");
+  }
+ }
+
+ public void elargir(){ //TP2 Question 4
+  Pixel[][] old = new Pixel[getLargeur()][getHauteur()];
+  old = tabPixel;
+  largeur=getLargeur()*2;
+  tabPixel = new Pixel[getLargeur()][getHauteur()];
+  for(int col=0;col<=tabPixel.length-1;col++) {
+   for(int ligne=0;ligne<=tabPixel[0].length-1;ligne++) {
+    if(col%2==0){
+     tabPixel[col][ligne]=new Pixel();
+     tabPixel[col][ligne]=old[col/2][ligne];
+    }
+    else{
+     tabPixel[col][ligne]=tabPixel[col-1][ligne];
+    }
+   }
+  }
+ }
+
+ //TP3
+ public void incrusterCoinGaucheVoid(Image avPlan){//TP3 Question 1 methode void modifier l'image
+  for (int col = 0; col < avPlan.getTabPixel().length; col++) {
+   for (int ligne = 0; ligne < avPlan.getTabPixel()[0].length; ligne++) {
+    if(avPlan.tabPixel[col][ligne].getAlpha()==0){
+     avPlan.tabPixel[col][ligne] = tabPixel[col][ligne];
+    }
+    else{
+     tabPixel[col][ligne]=avPlan.tabPixel[col][ligne];
+    }
+   }
+  }
+ }
+
+ public Image incrusterCoinGauche(Image avPlan){//TP3 Question 1 Retourne une nouvelle image avec incrustation
+  Image imageCG = new Image();
+  imageCG = copyImage();
+  for (int col = 0; col < avPlan.getTabPixel().length; col++) {
+   for (int ligne = 0; ligne < avPlan.getTabPixel()[0].length; ligne++) {
+    if(avPlan.tabPixel[col][ligne].getAlpha()==0){
+    }
+    else{
+     imageCG.tabPixel[col][ligne]=avPlan.tabPixel[col][ligne];
+    }
+   }
+  }
+  return imageCG;
+ }
+
+ public Image copyImage(){
+  Image temp = new Image();
+  temp.hauteur = getHauteur();
+  temp.largeur = getLargeur();
+  temp.tabPixel = new Pixel[getLargeur()][getHauteur()];
+
+  for(int col=0;col<=temp.largeur-1;col++){
+   for (int ligne=0;ligne<=temp.hauteur-1;ligne++){
+    temp.tabPixel[col][ligne]= new Pixel();
+    temp.tabPixel[col][ligne]=tabPixel[col][ligne];
+   }
+  }
+  return temp;
+ }
+
+ public Image incrusterCarré(){//TP3 Question 2
+  Image carre = new Image();
+  carre.hauteur = 100;
+  carre.largeur = 100;
+  carre.tabPixel = new Pixel[carre.getLargeur()][carre.getHauteur()];
+  for(int col=0;col<=carre.tabPixel.length-1;col++){
+   for (int ligne=0;ligne<=carre.tabPixel[0].length-1;ligne++){
+    carre.tabPixel[col][ligne]= new Pixel();
+    carre.tabPixel[col][ligne].setPixel(255, 238, 0, 255);
+   }
+  }
+  Image temp = new Image();
+  temp = copyImage();
+  for(int col = 100;col<=200;col++){
+   for(int ligne = 50;ligne<=150;ligne++){
+    temp.tabPixel[col][ligne]=carre.tabPixel[col-col][ligne-ligne];
+   }
+  }
+  return temp;
+ }
+
+ public Image incruster(Image avPlan, int x, int y){//TP3 Question 3
+  Image temp = new Image();
+  temp = copyImage();
+  for(int col = 0 ; col < avPlan.largeur && col<temp.largeur-x; col++){
+   if(col+x<0){
+    col = col++;
+   }
+   else{
+    for(int ligne = 0 ; ligne < avPlan.hauteur && ligne < temp.hauteur-y ; ligne++){
+     if(ligne+y<0){
+      ligne = ligne++;
+     }
+     else{
+      if(avPlan.tabPixel[col][ligne].getAlpha()==0) { 
+      }
+
+      else{
+       temp.tabPixel[col+x][ligne+y]=avPlan.tabPixel[col][ligne];
+      }
+     }
+    }
+   }
+  }
+  return temp;
+ }
+
+ public static void main(String[] args) {
+  Image prairie = new Image("images/extrait-prairie.png");
+  Image renard = new Image ("images/renard.png");
+
+  //Question 1
+  prairie.incrusterCoinGauche(renard).afficheImage();
+
+  //Question 2
+  Image perruche = new Image("images/perruche.png");
+  perruche.incrusterCarré().afficheImage();
+
+  //Question 3 et 4
+  prairie.incruster(renard,125,60).afficheImage();
+  prairie.incruster(renard,400,220).afficheImage();
+  prairie.incruster(renard, -200, 30).afficheImage();
+ }
+} 
---
+++ tp3/src/tp3/Pixel.java
@@ -0,0 +1,85 @@
+package tp3;
+public class Pixel {
+	private int rouge;
+	private int vert;
+	private int bleu;
+	private int alpha;
+
+	public Pixel(){
+		
+	}
+
+	public Pixel(int rouge, int vert, int bleu, int alpha){
+		this.rouge = verifValue(rouge);
+		this.vert = verifValue(vert);
+		this.bleu = verifValue(bleu);
+		this.alpha = verifValue(alpha);
+	}
+
+	public void setPixel(int rouge, int vert, int bleu, int alpha){
+		setRouge(rouge);
+		setVert(vert);
+		setBleu(bleu);
+		setAlpha(alpha);
+	}
+
+	public int verifValue(int valeur){
+		if(valeur<0||valeur>255){
+			throw new IllegalArgumentException ();
+		}
+		return valeur;
+	}
+
+	public int getRouge() {
+		return rouge;
+	}
+
+	public void setRouge(int rouge) {
+		this.rouge = verifValue(rouge);
+	}
+
+	public int getVert() {
+		return vert;
+	}
+
+	public void setVert(int vert) {	
+		this.vert = verifValue(vert);
+
+	}
+
+	public int getBleu() {
+		return bleu;
+	}
+
+	public void setBleu(int bleu) {
+		this.bleu = verifValue(bleu);
+	}
+
+	public int getAlpha() {
+		return alpha;
+	}
+
+	public void setAlpha(int alpha) {
+		this.alpha = verifValue(alpha);
+	}
+
+	public String toString(){
+		return "("+getRouge()+","+getVert()+","+getBleu()+","+getAlpha() +")";
+	}
+
+	public static void main(String[] args) {
+		try{	
+			Pixel p1 = new Pixel(150, 140, 200, 250);
+			Pixel p2 = new Pixel(240,45,56,250);
+			System.out.println(p1);
+			System.out.println(p2);
+			p1.setPixel(50, 50, 50, 50);
+			p2.setPixel(100, 100, 100, 100);
+			System.out.println(p1);
+			System.out.println(p2);			
+		}
+		catch(IllegalArgumentException e){
+			System.err.println("Erreur dans les paramètres du pixel");
+		}
+	}
+}
